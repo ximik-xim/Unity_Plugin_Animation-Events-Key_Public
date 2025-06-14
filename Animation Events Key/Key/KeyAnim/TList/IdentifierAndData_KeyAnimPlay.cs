0e57602cff1fcaedfd6bf46e25af9f0ec20af572
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-14 16:00:11 +0300

    - Добавил подержку новой версии TList(0.6.0) - Созданы бэкапы

--- Animation Events Key/Key/KeyAnim/TList/IdentifierAndData_KeyAnimPlay.cs
+++ Animation Events Key/Key/KeyAnim/TList/IdentifierAndData_KeyAnimPlay.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_KeyAnimPlay : AbsIdentifierAndData<IndifNameSO_Ke
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<KeyAnimPlay>(json);
+ }
+#endif
 }
