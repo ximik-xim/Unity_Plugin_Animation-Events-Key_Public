9c8b6d37e5fb11d1803b82cc7d65a6d241504e20
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-07-01 11:40:33 +0300

    - Исправил ошибку в наименовании SO - Решил удалить SO ключей, т.к бэкапы хорошо справляются

--- Animation Events Key/Key/KeyAnim/TList/SO_Data_KeyAnimPlay.cs
+++ Animation Events Key/Key/KeyAnim/TList/SO_Data_KeyAnimPlay.cs
@@ -1,6 +1,6 @@
 using UnityEngine; 
 using TListPlugin; 
-[CreateAssetMenu(menuName = "h")]
+[CreateAssetMenu(menuName = "Custom Anim Events/Key Anim")]
 public class SO_Data_KeyAnimPlay : Abs_SO_IdentifierAndData<IdentifierAndData_KeyAnimPlay,IndifNameSO_KeyAnimPlay,string,KeyAnimPlay>
 {
 
--- Animation Events Key/Key/KeyEvent/TList/SO_Data_KeyAnimEvent.cs
+++ Animation Events Key/Key/KeyEvent/TList/SO_Data_KeyAnimEvent.cs
@@ -1,6 +1,6 @@
 using UnityEngine; 
 using TListPlugin; 
-[CreateAssetMenu(menuName = "fg")]
+[CreateAssetMenu(menuName = "Custom Anim Events/Key Event")]
 public class SO_Data_KeyAnimEvent : Abs_SO_IdentifierAndData<IdentifierAndData_KeyAnimEvent,IndifNameSO_KeyAnimEvent,string,KeyAnimEvent>
 {
 
